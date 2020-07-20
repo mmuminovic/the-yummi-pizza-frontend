12a4c51c51c4cddd59d72802231428a34154e0b2
Muhamed Muminović <muminovic.muhamed01@gmail.com>
2020-07-20 22:48:29 +0200

    docs folder

---
+++ docs/precache-manifest.5dfd35a88a79017888aec3f620e12cf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b65cebe3f526c7e707c08c873db8ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45bd6855f6fe28db4535",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "83cb301850dc7b9b54c6",
+    "url": "./static/css/main.03e4e368.chunk.css"
+  },
+  {
+    "revision": "45bd6855f6fe28db4535",
+    "url": "./static/js/2.6543ed76.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "./static/js/2.6543ed76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83cb301850dc7b9b54c6",
+    "url": "./static/js/main.79be7c26.chunk.js"
+  },
+  {
+    "revision": "d50cffa359b5463cacfa",
+    "url": "./static/js/runtime-main.e038f1d6.js"
+  }
+]);
